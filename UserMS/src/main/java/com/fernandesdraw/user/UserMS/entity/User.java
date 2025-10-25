fd7484eeb04835181d34630c90eccc25a27cad3a
fernandes-draw <fernandes_draw@hotmail.com>
2025-10-25 20:41:38 -0300

    Created the user entity.

---
+++ UserMS/src/main/java/com/fernandesdraw/user/UserMS/entity/User.java
@@ -0,0 +1,27 @@
+package com.fernandesdraw.user.UserMS.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+  private String role;
+}
---
+++ UserMS/src/test/java/com/fernandesdraw/user/UserMS/UserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandesdraw.user.UserMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
